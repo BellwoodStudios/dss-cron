3fc7f4a646e7c6df958eea52b8bbbf360823fc81
Sam MacPherson <xylom44@gmail.com>
2020-12-25 08:23:42 -0500

    setup basics

--- src/DssCron.sol
+++ src/DssCron.sol
@@ -1,4 +1,28 @@
 pragma solidity ^0.6.7;
 
+import { VatAbstract } from "dss-interfaces/dss/VatAbstract.sol";
+
+// Allow placing bounties that increase over time on maintenance functions
+// that otherwise require altruistic keepers to maintain
+
 contract DssCron {
+
+    // --- Auth ---
+    mapping (address => uint256) public wards;
+    function rely(address usr) external auth { wards[usr] = 1; emit Rely(usr); }
+    function deny(address usr) external auth { wards[usr] = 0; emit Deny(usr); }
+    modifier auth { require(wards[msg.sender] == 1); _; }
+
+    VatAbstract immutable public vat;
+
+    // --- Events ---
+    event Rely(address indexed usr);
+    event Deny(address indexed usr);
+
+    constructor(address vat_) public {
+        wards[msg.sender] = 1;
+        emit Rely(msg.sender);
+        vat = VatAbstract(vat_);
+    }
+
 }
